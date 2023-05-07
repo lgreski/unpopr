135c51ca80f12c0193d7e408db00b2573c958505
Len Greski <len@greskilabs.com>
2023-05-07 15:39:18 -0400

    minor edit to test

--- tests/testthat/test-is_valid_url.R
+++ tests/testthat/test-is_valid_url.R
@@ -1,6 +1,6 @@
 test_that("is_valid_url() works", {
   expect_equal(is_valid_url("https://population.un.org"),TRUE)
-  expect_equal(is_valid_url("https://population.un.org/"),TRUE)
+  expect_equal(is_valid_url("http://population.un.org/"),TRUE)
   expect_equal(is_valid_url("https://population.un.us/zzz"),TRUE)
   expect_equal(is_valid_url("population.un.us/zzz"),FALSE)
 
