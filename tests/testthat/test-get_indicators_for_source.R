37192ff7f117ed0cd3bf72d0ce0d348c3f716d4c
Len Greski <len@greskilabs.com>
2024-06-17 07:01:30 -0400

    remove misplaced test

--- tests/testthat/test-get_indicators_for_source.R
+++ tests/testthat/test-get_indicators_for_source.R
@@ -1,7 +1,3 @@
 test_that("get_indicators_for_source() works", {
-
-  expect_warning(get_indicators_for_source("1"),"HTTP status was '404 Not Found'")
   expect_equal(nrow(get_indicators_for_source("25")),42)
-
-
 })
